d0f6692400a736cf624f1a0c0d25f455f9c19f9c
sbuendia <sbuendiaf@gmail.com>
2023-03-07 12:49:17 +0100

    partejs

---
+++ REINICIOPROYECTO/JS/01.js
@@ -0,0 +1,24 @@
+var producto = "Juego de mesa catan";
+
+var disponible;
+
+disponible = true;
+disponible = false;
+
+var producto1 = "Risk",
+    disponible1 = false,
+    categoria = "Juegos de mesa";
+
+console.log("PRODUCTO:",producto);
+
+var camiseta = "red",
+    pantalon = "yellow",
+    zapato = "blue";
+
+console.log("CAMISETA:", camiseta);
+console.log("PANTALON:", pantalon);
+console.log("ZAPATO:", zapato);
+
+const pi = "3.14";
+
+console.log("pi:", pi);
---
+++ REINICIOPROYECTO/JS/02.js
@@ -0,0 +1,51 @@
+// const producto = "Juego de mesa Catan";
+// const producto2 = String("Risk");
+// const producto3 = new String("Uno");
+
+// console.log("Producto:", producto);
+// console.log("Producto2:", producto2);
+// console.log("Producto3:", producto3);
+
+// console.log("TYPEOF Producto:",typeof producto);
+// console.log("TYPEOF Producto2:",typeof producto2);
+// console.log("TYPEOF Producto3:",typeof producto3);
+// .typeof nos indica que tipo de  variable
+
+// let numero100 = 100;
+// let numero200 = "200";
+
+// console.log("NUMERO100:", numero100);
+// console.log("NUMERO200:", numero200);
+
+// console.log("NUMERO100:",typeof numero100);
+// console.log("NUMERO200:", typeof numero200);
+
+// console.log("Producto:", producto.length);
+// .length nos mostrara la longitud de la variable
+
+// console.log("mesa:", producto.indexOf("mesa"));
+// .indexOf nos indica si lo que le preguntamos existe en nuestra variable y nos indica la posición.
+
+// console.log("mesa:", producto.includes("mesa"));
+// .includes nos indicara con true o false si dicho elemento se incluye en la variable.
+
+let numero1 = 100;
+let numero2 = 200.25;
+let numero3 = "200";
+let numero4 = 2/3;
+
+console.log("numero1:", numero1);
+console.log("numero2:", numero2);
+console.log("numero3:", numero3);
+console.log("numero4:", numero4);
+
+console.log("numero1:",numero1, typeof numero1);
+console.log("numero2:",numero2, typeof numero2);
+console.log("numero3:",numero3, typeof numero3);
+console.log("numero4:",numero4, typeof numero4);
+
+let numero5 = numero1+numero2;
+let numero6 = numero3+numero4;
+
+console.log("suma:", numero5)
+console.log("suma:", numero6)
